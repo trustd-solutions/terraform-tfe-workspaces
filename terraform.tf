8f8339ebfcadd79d8430e02e5734d55d3ba0353c
Vitor Carvalho <vcml@pm.me>
2021-11-23 19:22:22 -0300

    removing aws provider from required providers

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = "1.0.0"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.45.0"
-    }
     template = {
       source  = "hashicorp/template"
       version = "2.2.0"

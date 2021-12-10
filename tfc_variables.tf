ba94aca17587e4140a12e06cc06da982460810d6
Vitor Carvalho <vcml@pm.me>
2021-12-10 17:40:44 -0300

    specifying vendors

--- tfc_variables.tf
+++ tfc_variables.tf
@@ -31,3 +31,28 @@ resource "tfe_variable" "environment_variable" {
   category     = "env"
   workspace_id = tfe_workspace.this[each.value.workspace_name].id
 }
+
+# vendors specifics tfe_variable resources
+#resource "tfe_variable" "google_credential" {
+#for_each     = var.workspaces
+#key          = var.google_credentials
+#value        = var.google_credentials_value
+#category     = "env"
+#workspace_id = tfe_workspace.this[each.key].id
+#}
+
+resource "tfe_variable" "aws_key_id" {
+  for_each     = var.workspaces
+  key          = var.aws_access_key_id
+  value        = var.aws_access_key_id_value
+  category     = "env"
+  workspace_id = tfe_workspace.this[each.key].id
+}
+
+resource "tfe_variable" "aws_secret_access_key" {
+  for_each     = var.workspaces
+  key          = var.aws_secret_access_key
+  value        = var.aws_secret_access_key_value
+  category     = "env"
+  workspace_id = tfe_workspace.this[each.key].id
+}
--- variables.tf
+++ variables.tf
@@ -40,3 +40,36 @@ variable "terraform_cloud_token" {
   description = "The Team token used to authenticate with Terraform Cloud. See [Authentication](https://registry.terraform.io/providers/hashicorp/tfe/latest/docs#authentication) for more information"
   type        = string
 }
+
+variable "aws_access_key_id" {
+  description = "AWS_ACCESS_KEY_ID variable key"
+  type        = string
+  value       = "AWS_ACCESSS_KEY_ID"
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS_SECRET_ACCESS_KEY variable key"
+  type        = string
+  value       = "AWS_SECRET_ACCESSS_KEY"
+}
+
+variable "aws_access_key_id_value" {
+  description = "AWS_ACCESS_KEY_ID variable value key"
+  type        = string
+}
+
+variable "aws_secret_access_key_value" {
+  description = "AWS_SECRET_ACCESS_KEY variable value key"
+  type        = string
+}
+
+variable "google_credentials" {
+  description = "GOOGLE_CREDENTIALS variable key"
+  type        = string
+  value       = "GOOGLE_CREDENTIALS"
+}
+
+variable "google_credentials_value" {
+  description = "GOOGLE_CREDENTIALS variable value key"
+  type        = string
+}
